7dfb6a3293d15eeafdc6cd76d4d287f5c1a355f9
preetkaur <parampreetkaur059@gmail.com>
2026-02-12 18:26:12 +0530

    add new pages

--- src/components/AllProducts/AllProducts.jsx
+++ src/components/AllProducts/AllProducts.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CategoryPage from '../CategoryPage/CategoryPage'
 import bgFruits from '../../assets/all-banner.jpg'
+import { FaHome } from 'react-icons/fa'
 
 
 const AllProducts = () => {
   return (
     <div>
+      <FaHome/>
        <CategoryPage title = 'Fruits & Veggies' bgImage = {bgFruits} categories={['All']}/>
     </div>
   )
